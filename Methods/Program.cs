3d9b0c4e0a9b8d8861ca4ce3af87b30d8ad48130
AnıL <AnL@DESKTOP-THANGOR>
2023-10-24 17:37:47 +0300

    C# Dersleri 22: Parametreli Metotlarla Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -7,12 +7,20 @@
             Add();
             Add();
             Add();
+            int result = Add2(20, 30);
+            Console.WriteLine(result);
             Console.ReadLine();
 
         }
         static void Add()
         {
             Console.WriteLine("Added!");
+        }
+
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
         }
     }
 }
