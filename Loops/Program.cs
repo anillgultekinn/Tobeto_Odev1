312e93f8dadefed32993dc1d743381147395678b
AnıL <AnL@DESKTOP-THANGOR>
2023-10-24 23:26:04 +0300

    C# Dersleri 31: While Döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,7 +1,18 @@
 ﻿
 
-for (int i = 1; i <= 100; i+=2)
+static void ForLoop()
 {
-    Console.WriteLine(i);
+    for (int i = 0; i <= 0; i += 2)
+    {
+        Console.WriteLine(i);
+    }
+    Console.WriteLine("Finished!");
 }
-Console.WriteLine("Finished!");
+
+int number = 100;
+while (number >= 0)
+{
+    Console.WriteLine(number);
+    number--;
+}
+Console.WriteLine("Now number is {0}", number);
