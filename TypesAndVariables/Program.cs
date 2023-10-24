3c59d71004257c0f446b586ee6208a29684e7ac9
AnıL <AnL@DESKTOP-THANGOR>
2023-10-24 13:44:27 +0300

    C# Dersleri 6: Long Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,6 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-//Value Types
+﻿//Value Types
+
 int number1 = 2147483647;
+long number2 = 9223372036854775807;
 Console.WriteLine("Number 1 is {0}",number1);  
-Console.ReadLine();
+Console.WriteLine("Number 2 is {0}",number2);
