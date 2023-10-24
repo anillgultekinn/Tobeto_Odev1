da0c47569a41511cfc1af7a5458b55dca1733cb7
AnıL <AnL@DESKTOP-THANGOR>
2023-10-24 14:25:24 +0300

    C# Dersleri 14: Var Keyword İle Çalışmak

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -8,12 +8,18 @@ byte number4 = 255;
 short number3 = 32767;
 int number1 = 2147483647;
 long number2 = 9223372036854775807;
+var number7 = 10;
+number7 = 'A';
+//number7 = "A";
+
+
 Console.WriteLine("Number 1 is {0}",number1);  
 Console.WriteLine("Number 2 is {0}",number2);
 Console.WriteLine("Number 3 is {0}",number3);
 Console.WriteLine("Number 4 is {0}",number4);
 Console.WriteLine("Number 5 is {0}",number5);
 Console.WriteLine("Number 6 is {0}",number6);
+Console.WriteLine("Number 7 is {0}",number7);
 Console.WriteLine("Character is : {0}", (int)character);
 Console.WriteLine((int)Days.Friday);
 
