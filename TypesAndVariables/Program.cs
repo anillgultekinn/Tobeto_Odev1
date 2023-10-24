25a1793ae24d52abb1be66a98b0ab25705827e5a
AnıL <AnL@DESKTOP-THANGOR>
2023-10-24 13:49:39 +0300

    C# Dersleri 7: Short Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,6 +1,8 @@
 ﻿//Value Types
 
+short number3 = 32767;
 int number1 = 2147483647;
 long number2 = 9223372036854775807;
 Console.WriteLine("Number 1 is {0}",number1);  
 Console.WriteLine("Number 2 is {0}",number2);
+Console.WriteLine("Number 3 is {0}",number3);
