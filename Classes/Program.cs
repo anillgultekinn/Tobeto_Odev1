cb3c2ca00ba713dfbbeac78985879990f9139f6f
AnıL <AnL@DESKTOP-THANGOR>
2023-10-26 14:33:18 +0300

    C# Dersleri 38: Classlar İçinde Property Tanımlamak

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -12,6 +12,19 @@
             productManager.Add();
             productManager.Update();
 
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demiroğ";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                City = "İstanbul",
+                FirstName = "Derin",
+                LastName = "Demiroğ"
+            };
         }
     }
  
