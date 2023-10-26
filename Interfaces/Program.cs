d09a698eb61347dec67ea05268e717be9d4fb848
AnıL <AnL@DESKTOP-THANGOR>
2023-10-26 22:43:25 +0300

    C# Dersleri 41: İnterfacelere Temel Giriş

---
+++ Interfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        interface IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+
+
+        }
+        class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Address { get; set; }
+
+        }
+        class Student : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Deparmant { get; set; }
+        }
+    }
+}
